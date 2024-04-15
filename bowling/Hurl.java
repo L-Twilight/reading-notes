8bc42d4c9c048913f4d5d40caf049244a55bcf20
Parus Major <139199969+mjjianlong@users.noreply.github.com>
2024-04-16 04:19:42 +0800

    Create Hurl.java

---
+++ bowling/Hurl.java
@@ -0,0 +1,21 @@
+package com.risesun;
+
+
+/**
+ * 投递
+ */
+public class Hurl {
+    /**
+     * 击瓶数
+     */
+    private final int bottles;
+
+
+    public Hurl(int bottles) {
+        this.bottles = bottles;
+    }
+
+    public int getBottles(){
+        return bottles;
+    }
+}
